a21a9e4c42a0204993f925ddafd236c3f5d8a4de
Jose Abell <ja.abell@gmail.com>
2020-10-08 16:40:41 -0300

    First benchmark

---
+++ bench01.py
@@ -0,0 +1,22 @@
+from mpi4py import MPI
+import numpy as np
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+print(f"Hi from process {rank} of {size}.")
+
+N = 100
+
+if rank == 0:
+    A = np.random.rand(N,N)#, dtype=np.double)
+    for proc in range(1,size):
+        comm.send(A, dest=proc, tag=11)
+    Arecv = A
+else:
+    Arecv = comm.recv(source=0, tag=11)
+    print(f" --> process {rank} received a matriz of shape {Arecv.shape}")
+
+
+np.savez(f"A_{rank}_{size}.npz", A=Arecv)
